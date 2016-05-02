81be649f718ae71d418329015a0053ac9ad436e6
Jojooo0o <jonasdorsch@web.de>
2016-05-02 19:17:01 +0200

    Update tests.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -53,11 +53,15 @@ TEST_CASE("describe_vektor_div", "[div vec2]")
 {
 	Vec2 v{3.0f, 2.0f};
 	float w = 2.0f;
+	float z = 0.0f;
 
 	v / w;
+	v / z;
 
 	REQUIRE(Approx(1.5f) == v.x);
 	REQUIRE(Approx(1.0f) == v.y);
+	REQUIRE(Approx(0.0f) == v.x);
+	REQUIRE(Approx(0.0f) == v.y);
 }
 
 TEST_CASE("describe_2vektor_add", "[2add vec2]")
@@ -90,6 +94,54 @@ TEST_CASE("describe_2vektor_sub", "[2sub vec2]")
 
 	REQUIRE(Approx(2.5f) == (w - z).x);
 	REQUIRE(Approx(-1.5f) == (w - z).y);
+}
+
+TEST_CASE("describe_2vektor_multi", "[2multi vec2]")
+{
+	Vec2 v{1.5f, 2.5f};
+	Vec2 w{2.5f, 3.5f};
+	Vec2 z{0.0f, 5.0f};
+	float a = 2.0f;
+
+	REQUIRE(Approx(3.0f) == (v.x * a);
+	REQUIRE(Approx(5.0f) == (w.x * a);
+	REQUIRE(Approx(0.0f) == (z.x * a);
+	
+	REQUIRE(Approx(5.0f) == (v.y * a);
+	REQUIRE(Approx(7.0f) == (w.y * a);
+	REQUIRE(Approx(10.0f) == (z.y * a);
+}
+
+TEST_CASE("describe_2vektor_div", "[2div vec2]")
+{
+	Vec2 v{4.0f, 11.0f};
+	Vec2 w{2.0f, 7.0f};
+	Vec2 z{0.0f, 5.0f};
+	float a = 2.0f;
+
+	REQUIRE(Approx(2.0f) == (v.x / a);
+	REQUIRE(Approx(1.0f) == (w.x / a);
+	REQUIRE(Approx(0.0f) == (z.x / a);
+	
+	REQUIRE(Approx(5.5f) == (v.y / a);
+	REQUIRE(Approx(3.5f) == (w.y / a);
+	REQUIRE(Approx(2.5f) == (z.y / a);
+}
+
+TEST_CASE("describe_2vektor_multi2", "[2multi2 vec2]")
+{
+	float a = 4.0f;
+	Vec2 v{1.5f, 2.5f};
+	Vec2 w{2.5f, 3.5f};
+	Vec2 z{0.0f, 5.0f};
+
+	REQUIRE(Approx(6.0f) == (a * v.x);
+	REQUIRE(Approx(10.0f) == (a * w.x);
+	REQUIRE(Approx(0.0f) == (a * z.x);
+	
+	REQUIRE(Approx(10.0f) == (a * v.y);
+	REQUIRE(Approx(14.0f) == (a * w.y);
+	REQUIRE(Approx(20.0f) == (a * z.y);
 }
 
 int main(int argc, char *argv[])
